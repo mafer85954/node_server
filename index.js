3d08f978b4b7ee509aa6678ae96a2a7f376c95bf
mafer85954 <maryfertoledo22@gmail.com>
2022-06-29 17:59:53 -0500

    se crea el proyecto

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.set('port', 3002)
+
+//routes
+app.get('/', (req, res)=>{
+    res.send('hola mundo!!')
+})
+
+app.get('/persona', (req, res)=>{
+    res.send('hola, mi nombre es María Fernanda Toledo Coello y tengo 21 años. \n Nací un 22 de enero del 2001 y soy acuario')
+})
+
+app.listen(app.get('port'), ()=>{
+    console.log('Aplicacion corriendo en el puerto 3002')
+})
+
+
